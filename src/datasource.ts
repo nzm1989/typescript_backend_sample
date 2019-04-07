1fcf70af8c910f1ffb3ce30d2ab446375dce3ebc
Joseph <lunch_box57@hotmail.com>
2019-04-07 22:43:03 +0800

    db config updates

--- src/datasource.ts
+++ src/datasource.ts
@@ -3,9 +3,9 @@ import {Sequelize} from "sequelize-typescript";
 const datasource =  new Sequelize({
   database: "bidbidbidschema",
   dialect: "mysql",
-  username: "root",
-  password: "password",
-  storage: ":localhost:",
+  username: "bbb",
+  password: "PI9C3zZGFIigUA",
+  storage: "bbb-dev.crqowmd3qvd6.ap-southeast-1.rds.amazonaws.com",
   pool: {
     max: 10,
     min: 0,
